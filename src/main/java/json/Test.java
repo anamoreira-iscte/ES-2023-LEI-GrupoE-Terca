feb616d0fa039c083243665bbb3654fdc9c3caf7
ines-iscte <icaoo1@iscte-iul.pt>
2023-04-17 17:22:33 +0100

    Url csv

--- src/main/java/json/Test.java
+++ src/main/java/json/Test.java
@@ -7,5 +7,8 @@ public class Test {
         Functionalities func = new Functionalities();
         func.csvToJson();
         func.jsonToCsv();
+        CsvDownloader csvd = new CsvDownloader();
+        csvd.copyURLToFile("https://raw.githubusercontent.com/anamoreira-iscte/ES-2023-LEI-GrupoE-Terca/main/horario_exemplo.csv");
+
     }
 }

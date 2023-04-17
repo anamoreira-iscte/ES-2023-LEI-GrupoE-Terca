08c7e1fc211468420796dd871e37b184e25b8c7a
marianaiscte <mcgoo1@iscte-iul.pt>
2023-04-17 16:37:45 +0100

    Url csv done

---
+++ src/main/java/json/CsvDownloader.java
@@ -0,0 +1,49 @@
+package json;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Scanner;
+import java.net.URLEncoder;
+import java.io.FileWriter;
+
+public class CsvDownloader {
+
+    public static void copyURLToFile(String urls) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual é o Path do ficheiro csv onde pretende guardar?");
+        String path = scanner.nextLine();
+
+        FileWriter writer = new FileWriter(path);
+
+        try {
+            URL url = new URL(urls);
+            InputStream input = url.openStream();
+
+            char[] buffer = new char[4096];
+            int n = 0;
+            while (-1 != (n = new InputStreamReader(input).read(buffer))) {
+                writer.write(buffer,0,n);
+            }
+
+            input.close();
+            writer.close();
+
+            System.out.println("File downloaded successfully!");
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        copyURLToFile("https://raw.githubusercontent.com/anamoreira-iscte/ES-2023-LEI-GrupoE-Terca/main/horario_exemplo.csv");
+    }
+
+    }
